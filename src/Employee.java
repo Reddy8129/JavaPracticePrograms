3af8e81d66e0508597077b0678cc837d07a9dc0e
prashanth <reddy8129@gmail.com>
2022-04-16 08:12:36 +0530

    Employee class added

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+class Employee
+{
+	int emp_id;
+	String name;
+	int age;
+	public Employee(int emp_id, String name, int age)
+	{
+		this.emp_id=emp_id;
+		this.name=name;
+		this.age=age;
+	}
+	
+}
+
