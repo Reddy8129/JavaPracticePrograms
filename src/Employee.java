a41c613d4b8f45c4313ba15a7f785b965e76a50c
prashanth <reddy8129@gmail.com>
2022-04-16 08:22:40 +0530

    Added float salary

--- src/Employee.java
+++ src/Employee.java
@@ -3,11 +3,13 @@ class Employee
 	int emp_id;
 	String name;
 	int age;
-	public Employee(int emp_id, String name, int age)
+	float salary;
+	public Employee(int emp_id, String name, int age,float salary)
 	{
 		this.emp_id=emp_id;
 		this.name=name;
 		this.age=age;
+		this.salary=salary;	
 	}
 	
 }
