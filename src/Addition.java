30ceeb7443308de09e6550451b5e038ea2f4e002
prashanth <reddy8129@gmail.com>
2022-04-18 23:07:15 +0530

    method overloading demonstration

---
+++ src/Addition.java
@@ -0,0 +1,16 @@
+
+public class Addition {
+
+	int add(int a, int b, int c)
+	{
+		return a+b+c;
+	}
+	float add(float a, float b)
+	{
+		return a+b;
+	}
+	double add(double a, double b)
+	{
+		return a+b;
+	}
+}
