3da5dc82731c99f0c04dad9be4ed8ac8e52ad6bb
prashanth <reddy8129@gmail.com>
2022-04-27 21:50:07 +0530

    added a method to count num of character occurence in a String

---
+++ src/Words.java
@@ -0,0 +1,22 @@
+public class Words 
+{
+	static void countChars(String phrase)
+	{
+		char[] charstr= phrase.toLowerCase().toCharArray();
+		String alphabets = "abcdefghijklmnopqrstuvwxyz";
+		int[] count = new int[26];
+		for(int i=0;i<charstr.length;i++)
+		{
+			int idx= alphabets.indexOf(charstr[i]);
+			if(idx!=-1)
+				count[idx]=count[idx]+1;
+		}
+		for(int i=0;i<26;i++)
+		{
+			System.out.println(alphabets.charAt(i) +" --> " +count[i]);
+		}
+	}
+	public static void main(String[] args) {
+		countChars("Prashantha Reddy!?123"); 
+	}
+}
